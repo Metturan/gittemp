ffec614e9c545f0733c8a13fe98ee919a687d28d
Metturan Ketheswaran <metturanketh@gmail.com>
2017-11-29 13:37:24 -0500

    mobile overflow

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
     	<BrowserRouter>
-	      <div>
+	      <div className="clearWrapper">
 	      	<ScrollToTop>
 		        <Route exact path="/" component={Landing}/>
 		        <Route exact path="/company" component={Company} />
