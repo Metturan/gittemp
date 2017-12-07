7ea21ca83fe49e0c07144cf81ae0bc642018b166
Metturan Ketheswaran <metturanketh@gmail.com>
2017-12-07 10:21:19 -0500

    debounce

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -20,7 +20,7 @@ export function scrollBehaviour() {
 }
 
 export function showUp() {
-	function debounce(func, wait = 12, immediate = true) {
+	function debounce(func, wait = 15, immediate = true) {
 		var timeout;
 		return function() {
 			var context = this, args = arguments;
